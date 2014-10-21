49b93cc8d012753cf2673c566772ae9ddc43df34
Code Division <codedivision@11th.ventures>
2014-10-21 13:25:43 +0800

    tasks on tangan

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'faker'
   20.times do
-    Task.create(:task => Faker::Lorem.sentence)
-    Task.create(:started_at => rand(1.year).from_now)
-    Task.create(:completed_at => rand(2.year).from_now)
+    Task.create(task: Faker::Lorem.sentence,
+                started_at: rand(1.year).from_now,
+                completed_at:rand(2.year).from_now)
   end
